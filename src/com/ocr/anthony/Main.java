9d52f4989b19ef25f609808277d2cbf19be2c391
TheoM <theomercier@icloud.com>
2018-09-10 22:01:51 -0700

    Add text : order

--- src/com/ocr/anthony/Main.java
+++ src/com/ocr/anthony/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Order order = new Order();
         order.displayAvailableMenu();
         Scanner sc = new Scanner(System.in);
--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -1,17 +1,32 @@
 package com.ocr.anthony;
 
+import java.util.Scanner;
+
 public class Order {
     /**
      * Display all available menus in the restaurant.
      */
-    public void displayAvailableMenu() {
+    public void displayAvailableMenu()
+    {
+
+        System.out.println("Choix menu");
+        System.out.println("1 - Poulet");
+        System.out.println("2 - Boeuf");
+        System.out.println("3 - Végétarien");
+        System.out.println("Que souhaitez-vous comme menu ?");
 
     }
+
     /**
      * Display a selected menu.
      * @param nbMenu The selected menu.
      */
-    public void displaySelectedMenu(int nbMenu) {
 
+    public void displaySelectedMenu(int nbMenu)
+    {
+        /*Scanner sc = new Scanner(System.in);
+        int nbMenu = sc.nextInt();*/
+
+        System.out.println("Vous avez choisi le menu : " +nbMenu);
     }
 }
